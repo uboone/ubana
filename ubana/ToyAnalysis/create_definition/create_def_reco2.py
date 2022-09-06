b36bf17bcde373d993882530d08f8712eaec360a
cthorpe123 <c.thorpe@lancaster.ac.uk>
2022-09-06 15:46:24 -0500

    Adding create definition script.

---
+++ ubana/ToyAnalysis/create_definition/create_def_reco2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+
+project="make_events_BNB_TEST"
+stage="reco2"
+stage_name=stage
+usr="cthorpe"
+
+common="file_type mc and ub_project.version prod_v08_00_00_61 and ub_project.name "
+
+defname    = ["reco2"]
+fileformat = ["artroot"]
+
+for i in range(len(defname)):
+    thisdefname = usr + project+"_"+stage_name+"_"+ defname[i]
+    print thisdefname
+    cmd1 = "samweb delete-definition " + thisdefname
+    os.system(cmd1)
+    cmd = "samweb create-definition " + thisdefname
+    cmd = cmd + " '" + common + project + " and file_format " +fileformat[i] + " and ub_project.stage " + stage
+    cmd = cmd + " with availability physical'"
+    print cmd
+    os.system(cmd)
+    count_cmd = "samweb count-definition-files " + thisdefname
+    os.system(count_cmd)
+    #print thisdefname
+
+for i in range(len(defname)):
+    thisdefname = "cthorpe_" + project+ "_" +stage_name+"_" + defname[i]
+    print thisdefname
+
+print "Done!"
