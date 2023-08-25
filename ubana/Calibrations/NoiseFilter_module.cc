dcbf2facd60abd05b6da065e5b0dd6c16c80924c
Lynn Garren <garren@fnal.gov>
2023-08-24 19:15:28 -0500

    fix nonstandard indent

--- ubana/Calibrations/NoiseFilter_module.cc
+++ ubana/Calibrations/NoiseFilter_module.cc
@@ -271,7 +271,7 @@ namespace calibration {
                 waveNoiseHists[waveNoiseCounter]->SetBinContent(s+1, fHistMod->GetBinContent(s+1) );
 
             //do correlated noise removal
-      		if(waveNoiseCounter == waveNoiseGroupNum-1)
+    	    if(waveNoiseCounter == waveNoiseGroupNum-1)
       		{
                 if( fRunWaveFilterAlg == 1)
                     WaveFilterAlg(waveNoiseHists);
